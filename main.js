2b16b181df47ea45c8bd7c7479fc376af5253fe6
samandar-dev <samandarbohodirov01@gmail.com>
2022-04-22 08:00:18 +0500

    add

---
+++ main.js
@@ -0,0 +1,48 @@
+// masala 206 ==========================
+
+// function isPrimeNumber(n) {
+//   let son = 0;
+//   for (let i = 0; i <= n; i++) {
+//     let nat = 0
+//     for (let j = 0; j <= i; j++) {
+//       if (i % j == 0) {
+//         nat += 1
+//       }
+//     }
+//     if (nat == 2) {
+//       son++
+//     }
+//   }
+//   return son
+// }
+
+// console.log(isPrimeNumber(100));
+
+
+
+// masala 208 =======================
+
+// let str = "JOKE☺aaaa";
+
+// function getCharacterCount(s) {
+//   let n = s.split('');
+//   return n.length;
+// }
+
+// console.log(getCharacterCount(str));
+
+
+
+// masala 209 =======================
+
+// let str = "Berilgan soni: 19ta"
+
+// function parseInt(s) {
+//   let n = s.split('');
+//   n.forEach(elem => {
+//     if(elem.join('') == is)
+//   });
+//   console.log(n);
+// }
+
+// console.log(parseInt(str));
